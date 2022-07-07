cd9a12023348dc14cb47c67086e1dcb35ffff7ee
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2022-07-07 19:35:35 +0100

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,17 @@ import { execSync } from 'child_process';
 import * as path from 'path';
 import AdmZip = require('adm-zip');
 
-function getList(installedVSCCMLinuxLocally: boolean): string[] | undefined {
+function getList(installedVSCCMNotWinLocally: boolean): string[] | undefined {
 	if (vscode.workspace.workspaceFolders === undefined) {
 		return undefined;
 	}
-	let stdoutText: string = execSync(`${installedVSCCMLinuxLocally ? "./" : ""}vsccm list --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath}`).toString();	
+	let stdoutText: string = execSync(`${installedVSCCMNotWinLocally ? "./" : ""}vsccm list --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath}`).toString();	
 	let list: string[] = stdoutText.split("\n").filter(x => x[0] === "|").map(x => x.slice(2, x[x.length - 1] === "\r" ? x.length - 1 : x.length));
 	return list;
 }
 
-function getInstallList(installedVSCCMLinuxLocally: boolean): string[] {
-	let stdoutText: string = execSync(`${installedVSCCMLinuxLocally ? "./" : ""}vsccm install list`).toString();	
+function getInstallList(installedVSCCMNotWinLocally: boolean): string[] {
+	let stdoutText: string = execSync(`${installedVSCCMNotWinLocally ? "./" : ""}vsccm install list`).toString();	
 	let list: string[] = stdoutText.split("\n").filter(x => x[0] === "|").map(x => x.slice(2, x[x.length - 1] === "\r" ? x.length - 1 : x.length));
 	return list;
 }
@@ -22,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "vscode-configs-manager-extension" is now active!');
 
 	let installedVSCCM: boolean;
-	let installedVSCCMLinuxLocally: boolean;
+	let installedVSCCMNotWinLocally: boolean;
 
 	try {
 		execSync("vsccm");
@@ -35,15 +35,15 @@ export function activate(context: vscode.ExtensionContext) {
 	if (!installedVSCCM){
 		try {
 			execSync("./vsccm");
-			installedVSCCMLinuxLocally = true;
+			installedVSCCMNotWinLocally = true;
 		}
 		catch {
-			installedVSCCMLinuxLocally = false;
+			installedVSCCMNotWinLocally = false;
 		}
 	}
 
 	let commndSetup = vscode.commands.registerCommand('vscode-configs-manager-extension.setup', () => {
-		if (installedVSCCM || installedVSCCMLinuxLocally) {
+		if (installedVSCCM || installedVSCCMNotWinLocally) {
 			vscode.window.showWarningMessage("VSCCM is already installed");
 		}
 		else {
@@ -58,17 +58,23 @@ export function activate(context: vscode.ExtensionContext) {
 				zip.extractAllTo(process.cwd(), true);
 				execSync('chmod +x vsccm');
 				vscode.window.showInformationMessage("VSCCM installed");
-				installedVSCCMLinuxLocally = true;
+				installedVSCCMNotWinLocally = true;
+			}
+			else if (process.platform === 'darwin') {
+				let zip: AdmZip = new AdmZip(`${path.resolve(__dirname, '..')}/base-builds/base-build-macos.zip`);
+				zip.extractAllTo(process.cwd(), true);
+				vscode.window.showInformationMessage("VSCCM installed");
+				installedVSCCMNotWinLocally = true;
 			}
 			else {
-				vscode.window.showInformationMessage("We are so soryy, but there is no build for your OS. Wait for the new version of extension");
+				vscode.window.showInformationMessage("We are so sorry, but there is no build for your OS. Wait for the new version of extension");
 			}
 		}
 	});
 
 	let commandList = vscode.commands.registerCommand('vscode-configs-manager-extension.list', () => {
-		if (installedVSCCM || installedVSCCMLinuxLocally) {
-			let list: string[] | undefined = getList(installedVSCCMLinuxLocally);
+		if (installedVSCCM || installedVSCCMNotWinLocally) {
+			let list: string[] | undefined = getList(installedVSCCMNotWinLocally);
 			if (list === undefined) {
 				vscode.window.showErrorMessage("You haven't opened folder");
 			}
@@ -83,12 +89,12 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	let commandRemove = vscode.commands.registerCommand('vscode-configs-manager-extension.remove', () => {
-		if (installedVSCCM || installedVSCCMLinuxLocally) {
+		if (installedVSCCM || installedVSCCMNotWinLocally) {
 			if (vscode.workspace.workspaceFolders === undefined) {
 				vscode.window.showErrorMessage("You haven't opened folder");
 			}
 			else {
-				vscode.window.showInformationMessage(execSync(`${installedVSCCMLinuxLocally ? "./" : ""}vsccm remove --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath}`).toString());	
+				vscode.window.showInformationMessage(execSync(`${installedVSCCMNotWinLocally ? "./" : ""}vsccm remove --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath}`).toString());	
 			}
 		}
 		else {
@@ -98,12 +104,12 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	let commandClear = vscode.commands.registerCommand('vscode-configs-manager-extension.clear', () => {
-		if (installedVSCCM || installedVSCCMLinuxLocally) {
+		if (installedVSCCM || installedVSCCMNotWinLocally) {
 			if (vscode.workspace.workspaceFolders === undefined) {
 				vscode.window.showErrorMessage("You haven't opened folder");
 			}
 			else {
-				vscode.window.showInformationMessage(execSync(`${installedVSCCMLinuxLocally ? "./" : ""}vsccm clear --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath}`).toString());	
+				vscode.window.showInformationMessage(execSync(`${installedVSCCMNotWinLocally ? "./" : ""}vsccm clear --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath}`).toString());	
 			}
 		}
 		else {
@@ -113,8 +119,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	
 	let commandInstallList = vscode.commands.registerCommand('vscode-configs-manager-extension.installList', () => {
-		if (installedVSCCM || installedVSCCMLinuxLocally) {
-			vscode.window.showInformationMessage(`Your configs: ${getInstallList(installedVSCCMLinuxLocally).join(", ")}`);
+		if (installedVSCCM || installedVSCCMNotWinLocally) {
+			vscode.window.showInformationMessage(`Your configs: ${getInstallList(installedVSCCMNotWinLocally).join(", ")}`);
 		}
 		else {
 			vscode.window.showErrorMessage("VSCCM is not installed");
@@ -123,12 +129,12 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	let install = vscode.commands.registerCommand('vscode-configs-manager-extension.install', () => {
-		if (installedVSCCM || installedVSCCMLinuxLocally) {
+		if (installedVSCCM || installedVSCCMNotWinLocally) {
 			if (vscode.workspace.workspaceFolders === undefined) {
 				vscode.window.showErrorMessage("You haven't opened folder");
 			}
 			else {
-				let options: vscode.QuickPickItem[] = getInstallList(installedVSCCMLinuxLocally).map((label: string) => ({label}));
+				let options: vscode.QuickPickItem[] = getInstallList(installedVSCCMNotWinLocally).map((label: string) => ({label}));
 				let quickPick: vscode.QuickPick<vscode.QuickPickItem> = vscode.window.createQuickPick();
 				quickPick.items = options;
 				quickPick.onDidChangeSelection(([{label}]) => {
@@ -137,7 +143,7 @@ export function activate(context: vscode.ExtensionContext) {
 						vscode.window.showErrorMessage("You haven't opened folder");
 					}
 					else {
-						let stdoutText: string = execSync(`${installedVSCCMLinuxLocally ? "./" : ""}vsccm install --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath} ${label}`).toString();
+						let stdoutText: string = execSync(`${installedVSCCMNotWinLocally ? "./" : ""}vsccm install --workfolder=${vscode.workspace.workspaceFolders[0].uri.fsPath} ${label}`).toString();
 						for (let text of stdoutText.split("\n")) {
 							vscode.window.showInformationMessage(text);
 						}
